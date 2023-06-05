78a81b07bd3f594ec0a6739adc1b877bdd8fa543
Lingeng <xk21wl@163.com>
2023-06-05 14:45:15 +0800

    update decoder

--- src/layers/decoder.py
+++ src/layers/decoder.py
@@ -4,8 +4,9 @@ import torch.nn as nn
 
 class LSTMDecoder(nn.Module):
 
-    def __init__(self, encoder_dim, decoder_dim, vocab_size):
+    def __init__(self, encoder_dim, decoder_dim, vocab_size, tokenizer):
         super(LSTMDecoder, self).__init__()
+        self.tokenizer = tokenizer
         self.lstm = nn.LSTM(input_size=encoder_dim, hidden_size=decoder_dim, num_layers=1)
         self.fc = nn.Linear(decoder_dim, vocab_size)
         self.softmax = nn.LogSoftmax(dim=-1)
